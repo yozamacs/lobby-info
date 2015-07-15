9900e45b037f19e11214f36936c2dac5b4e3b54d
Maryum <maryum.styles@gmail.com>
2015-07-15 11:34:27 -0700

    remove http header from API url

--- app/assets/javascript/services/contributors.js
+++ app/assets/javascript/services/contributors.js
@@ -1,5 +1,5 @@
 angular.module("PoliticiansLibrary").factory('Contributors', function($resource){
-  return $resource("https://transparencydata.com/api/1.0/aggregates/pol/:cont_id/contributors.json?cycle=:year&limit=9&apikey=a100f4c82be646c4a94225263b9eec5e",
+  return $resource("//transparencydata.com/api/1.0/aggregates/pol/:cont_id/contributors.json?cycle=:year&limit=9&apikey=a100f4c82be646c4a94225263b9eec5e",
   {cont_id: '@cont_id', year: '@year',callback: "JSON_CALLBACK"},
   {getJSONP:{method: "JSONP",isArray: true}
   });
--- app/assets/javascript/services/politicians.js
+++ app/assets/javascript/services/politicians.js
@@ -1,5 +1,5 @@
 angular.module("PoliticiansLibrary").factory('Politicians', function($resource){
-  return $resource("https://transparencydata.com/api/1.0/aggregates/pols/top_9.json?cycle=:year&apikey=a100f4c82be646c4a94225263b9eec5e",
+  return $resource("//transparencydata.com/api/1.0/aggregates/pols/top_9.json?cycle=:year&apikey=a100f4c82be646c4a94225263b9eec5e",
   {year: '@year',callback: "JSON_CALLBACK"},
   {getJSONP:{method: "JSONP",isArray: true}
 });
