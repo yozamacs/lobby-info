ca6c59e1fb1ef210b0e2591b8e7995e32e01eb22
Maryum <maryum.styles@gmail.com>
2015-07-14 20:17:17 -0700

    it works :)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-// app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 app.listen(process.env.PORT || 3000);
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.redirect('/app/views');
 });
